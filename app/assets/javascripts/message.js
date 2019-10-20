d2cf57d2e0794503543f8087315938deaf8afe23
hakryun <hangnyeon@gmail.com>
2019-10-20 17:39:41 +0900

    redo

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,5 @@
 $(document).on('turbolinks:load', function() {
+
   function buildHTML(message){
     if (message.image_url !== null){
       var html = `<div class="message">
@@ -6,55 +7,54 @@ $(document).on('turbolinks:load', function() {
                       <div class="upper-message__user-name">
                         ${message.user_name}
                       </div>                      
-
                       <div class="upper-message__date">
                         ${message.created_at}
                       </div>          
                     </div>
-
                     <div class="lower-message">
                       <p class="lower-message__content">
                         ${message.content} 
                       </p>                      
-
                       <img src="${message.image_url}" class='lower-message__image'>
                     </div>
                   </div>`
     }else{      
       var html = `<div class="message">
-                    <div class="upper-message">
+
+                  <div class="upper-message">
 
                     <div class="upper-message__user-name">
                         ${message.user_name}                        
                       </div>
+
                       <div class="upper-message__date">
                         ${message.created_at}
                       </div>                      
                     </div>
-
                     <div class="lower-message">
                       <p class="lower-message__content">
                         ${message.content} 
-
                         </p>
                    </div>
                   </div>`;
     }
+
     return html;
   }
   $('.new_message').on('submit', function(e){
     e.preventDefault();
+
     var formData = new FormData(this); 
     var url = $(this).attr('action')
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
-
       dataType: 'json',
       processData: false,
       contentType: false
     })
+
     .done(function(data){
       var html = buildHTML(data);
       $('.messages').append(html)
@@ -64,9 +64,11 @@ $(document).on('turbolinks:load', function() {
         scrollTop: $('.messages')[0].scrollHeight
       })
     })
+
     .fail(function(){
       alert('error');
     })
+
     .always(function(){
       $('.form__submit').removeAttr('disabled');
     })
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,5 @@
 class MessagesController < ApplicationController
- 
-  before_action :set_group
+   before_action :set_group
 
   def index
     @message = Message.new
@@ -9,6 +8,7 @@ class MessagesController < ApplicationController
 
   def create
     @message = @group.messages.new(message_params)
+
     if @message.save    
       respond_to do |format|
         format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
